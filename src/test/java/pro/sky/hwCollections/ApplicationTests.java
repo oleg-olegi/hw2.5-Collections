2826733fadf200be414cf0cddf0f13f0de7c8763
User <ol.shink87@gmail.com>
2023-09-19 19:51:05 +0300

    initial commit

---
+++ src/test/java/pro/sky/hwCollections/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hwCollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

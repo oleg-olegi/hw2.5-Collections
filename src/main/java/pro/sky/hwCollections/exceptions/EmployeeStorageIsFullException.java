fbe4a29da9549b197564ea1503e9dbd11213e92c
User <ol.shink87@gmail.com>
2023-09-22 08:41:53 +0300

    все переделал вообще

--- src/main/java/pro/sky/hwCollections/exceptions/EmployeeStorageIsFullException.java
+++ src/main/java/pro/sky/hwCollections/exceptions/EmployeeStorageIsFullException.java
@@ -1,9 +1,5 @@
 package pro.sky.hwCollections.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class EmployeeStorageIsFullException extends RuntimeException {
     public EmployeeStorageIsFullException(String message) {
         super(message);
